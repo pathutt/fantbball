341f523d92d37411537d400dc889094a39539d70
pathutt <pthutt@outlook.com>
2020-04-27 17:18:38 -0400

    "ADDED JQUERY FUNCTIONALITY FOR PITCHER TABLE"

--- static/js/home.js
+++ static/js/home.js
@@ -18,6 +18,16 @@ function showRows(target){
     });
 }
 
+function showHitters(){
+    $("#pitcher_table").hide();
+    $("#hitter_table").show();
+}
+
+function showPitchers(){
+    $("#hitter_table").hide();
+    $("#pitcher_table").show();
+}
+
 $(document).ready(function() {
     // Change active table
     $(".nav-link").click(function () {
@@ -27,9 +37,16 @@ $(document).ready(function() {
 
     $("#hitters").click(function () {
         showAllRows();
+        showHitters();
+    });
+
+    $("#pitchers").click(function () {
+        showAllRows();
+        showPitchers();
     });
 
     $("#catcher").click(function () {
+        showHitters();
         hideRows();
         $('table tbody tr').each(function () {
             if($(this).find('td').eq(4).text() == "C"){
@@ -39,26 +56,31 @@ $(document).ready(function() {
     });
 
     $("#1b").click(function () {
+        showHitters();
         hideRows();
         showRows("1B");
     });
 
     $("#2b").click(function () {
+        showHitters();
         hideRows();
         showRows("2B");
     });
 
     $("#3b").click(function () {
+        showHitters();
         hideRows();
         showRows("3B");
     });
 
     $("#ss").click(function () {
+        showHitters();
         hideRows();
         showRows("SS");
     });
 
     $("#of").click(function () {
+        showHitters();
         hideRows();
         showRows("LF");
         showRows("CF");
@@ -66,20 +88,35 @@ $(document).ready(function() {
     });
 
     $("#dh").click(function (){
+        showHitters();
         hideRows();
         showRows("DH");
     });
 
     $("#mi").click(function () {
+        showHitters();
         hideRows();
         showRows("2B");
         showRows("SS");
     });
 
     $("#ci").click(function () {
+        showHitters();
         hideRows();
         showRows("1B");
         showRows("3B");
+    });
+
+    $("#sp").click(function () {
+        showPitchers();
+        hideRows();
+        showRows("SP");
+    });
+
+    $("#rp").click(function () {
+        showPitchers();
+        hideRows();
+        showRows("RP");
     });
 
     $('tbody tr').click(function (){
